c75ed23563914b0934b10306c8fb6334e8d1e36a
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2020-11-09 18:42:16 -0300

    Definido modelo de negocio da aplicacao
    Adicionada as classes Snack e Category

--- SnackHouse/SnackHouse/Controllers/HomeController.cs
+++ SnackHouse/SnackHouse/Controllers/HomeController.cs
@@ -26,12 +26,6 @@ namespace SnackHouse.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        } 
     }
 }
---
+++ SnackHouse/SnackHouse/Models/Snack.cs
@@ -0,0 +1,20 @@
+﻿namespace SnackHouse.Models
+{
+    public class Snack
+    {
+        //EF Core entende que nome da classe mais Id é chave primária ou só Id
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SmallDescription { get; set; }
+        public string LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImgUrl { get; set; }
+        public string ImgThumbnailUrl { get; set; }
+        public bool IsPreferSnack { get; set; }
+        public bool InStock { get; set; }
+        //Para EF saber que há o relacionamento entre classes, tem que haver o nome da classe mais Id
+        public int CategoryId { get; set; }
+        //Essa propriedade virtual define a propriedade de navegação (ele tem que saber que existe um relacionamento)
+        public virtual Category Category { get; set; }
+    }
+}
