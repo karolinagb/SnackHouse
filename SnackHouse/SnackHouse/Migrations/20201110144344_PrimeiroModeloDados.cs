e97e1b32486b28e7fc8cee6a42fc59d5bb7dedbe
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2020-11-10 18:54:30 -0300

    Adicionada string de conexao e informada a classe Context no Satartup com o provedor do banco

---
+++ SnackHouse/SnackHouse/Migrations/20201110144344_PrimeiroModeloDados.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SnackHouse.Migrations
+{
+    public partial class PrimeiroModeloDados : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Snacks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    SmallDescription = table.Column<string>(nullable: true),
+                    LongDescription = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    ImgUrl = table.Column<string>(nullable: true),
+                    ImgThumbnailUrl = table.Column<string>(nullable: true),
+                    IsPreferSnack = table.Column<bool>(nullable: false),
+                    InStock = table.Column<bool>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Snacks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Snacks_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Snacks_CategoryId",
+                table: "Snacks",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Snacks");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
--- SnackHouse/SnackHouse/Startup.cs
+++ SnackHouse/SnackHouse/Startup.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SnackHouse.Data;
 
 namespace SnackHouse
 {
@@ -24,6 +21,9 @@ namespace SnackHouse
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //Informando a classe DBCONtext utilizada e o provedor do banco
+            services.AddDbContext<SnackHouseDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
