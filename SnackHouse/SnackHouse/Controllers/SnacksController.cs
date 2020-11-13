b796782e9a7e2ad322cdb9fc167b69ca98b15f7d
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2020-11-13 12:21:00 -0300

    Criado Controller e View para modelo Snack
    Foi adicionada imagens dos alnches ao porjeto onde as mesmas estão sendo salvas no banco através da atualização da migration de População do banco

---
+++ SnackHouse/SnackHouse/Controllers/SnacksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SnackHouse.Repositories;
+
+namespace SnackHouse.Controllers
+{
+    public class SnacksController : Controller
+    {
+        private readonly ISnackRepository _snackRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public SnacksController(ISnackRepository snackRepository, ICategoryRepository categoryRepository)
+        {
+            _snackRepository = snackRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult FindAll()
+        {
+            var snacks = _snackRepository.FindAll();
+            return View(snacks);
+        }
+    }
+}
--- SnackHouse/SnackHouse/Migrations/20201111115900_PopularTabelas.cs
+++ SnackHouse/SnackHouse/Migrations/20201111115900_PopularTabelas.cs
@@ -7,22 +7,22 @@ namespace SnackHouse.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql("INSERT INTO Categories" +
-                "(Id, CategoryName,Description) VALUES(1,'Normal','Lanche feito com ingredientes normais')");
+                "(CategoryName,Description) VALUES('Normal','Lanche feito com ingredientes normais')");
             migrationBuilder.Sql("INSERT INTO Categories" +
-                "(Id,CategoryName,Description) VALUES(2,'Natural','Lanche feito com ingredientes integrais e naturais')");
+                "(CategoryName,Description) VALUES('Natural','Lanche feito com ingredientes integrais e naturais')");
 
             migrationBuilder.Sql("INSERT INTO Snacks" +
                 "(CategoryId,SmallDescription,LongDescription,InStock,ImgThumbnailUrl,ImgUrl,IsPreferSnack,Name,Price) " +
-                "VALUES(1,'Pão, hambúrger, ovo, presunto, queijo e batata palha','Delicioso pão de hambúrger com ovo frito; presunto e queijo de primeira qualidade acompanhado com batata palha',1, 'http://www.macoratti.net/Imagens/lanches/cheesesalada1.jpg','http://www.macoratti.net/Imagens/lanches/cheesesalada1.jpg', 0 ,'Cheese Salada', 12.50)");
+                "VALUES(1,'Pão, hambúrger, ovo, presunto, queijo e batata palha','Delicioso pão de hambúrger com ovo frito; presunto e queijo de primeira qualidade acompanhado com batata palha',1, 'cheesesalada1.jpg','cheesesalada1.jpg', 0 ,'Cheese Salada', 12.50)");
             migrationBuilder.Sql("INSERT INTO Snacks" +
                 "(CategoryId,SmallDescription,LongDescription,InStock,ImgThumbnailUrl,ImgUrl,IsPreferSnack,Name,Price) " +
-                "VALUES(1,'Pão, presunto, mussarela e tomate','Delicioso pão francês quentinho na chapa com presunto e mussarela bem servidos com tomate preparado com carinho.',1,'http://www.macoratti.net/Imagens/lanches/mistoquente4.jpg','http://www.macoratti.net/Imagens/lanches/mistoquente4.jpg',0,'Misto Quente', 8.00)");
+                "VALUES(1,'Pão, presunto, mussarela e tomate','Delicioso pão francês quentinho na chapa com presunto e mussarela bem servidos com tomate preparado com carinho.',1,'mistoquente4.jpg','mistoquente4.jpg',0,'Misto Quente', 8.00)");
             migrationBuilder.Sql("INSERT INTO Snacks" +
                 "(CategoryId,SmallDescription,LongDescription,InStock,ImgThumbnailUrl,ImgUrl,IsPreferSnack,Name,Price) " +
-                "VALUES(1,'Pão, hambúrger, presunto, mussarela e batalha palha','Pão de hambúrger especial com hambúrger de nossa preparação e presunto e mussarela; acompanha batata palha.',1,'http://www.macoratti.net/Imagens/lanches/cheeseburger1.jpg','http://www.macoratti.net/Imagens/lanches/cheeseburger1.jpg',1,'Cheese Burger', 11.00)");
+                "VALUES(1,'Pão, hambúrger, presunto, mussarela e batalha palha','Pão de hambúrger especial com hambúrger de nossa preparação e presunto e mussarela; acompanha batata palha.',1,'cheeseburger1.jpg','cheeseburger1.jpg',1,'Cheese Burger', 11.00)");
             migrationBuilder.Sql("INSERT INTO Snacks" +
                 "(CategoryId,SmallDescription,LongDescription,InStock,ImgThumbnailUrl,ImgUrl,IsPreferSnack,Name,Price) " +
-                "VALUES(2,'Pão Integral, queijo branco, peito de peru, cenoura, alface, iogurte','Pão integral natural com queijo branco, peito de peru e cenoura ralada com alface picado e iorgurte natural.',1,'http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg','http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg',0,'Lanche Natural Peito Peru', 15.00)");
+                "VALUES(2,'Pão Integral, queijo branco, peito de peru, cenoura, alface, iogurte','Pão integral natural com queijo branco, peito de peru e cenoura ralada com alface picado e iorgurte natural.',1,'lanchenatural.jpg','lanchenatural.jpg',0,'Lanche Natural Peito Peru', 15.00)");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
