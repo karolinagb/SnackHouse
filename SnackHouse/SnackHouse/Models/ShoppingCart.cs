d9d3ca0e0c174525a8b5e656f601311b89a96c4e
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2020-11-15 22:38:53 -0300

    Criada classe ShoopingCart
    Essa classe será carregada em memória para criação da sessão. Foi adicionado o método GetCart para obter um carrinho onde temos a criação da sessão a partir do contexto atual.

---
+++ SnackHouse/SnackHouse/Models/ShoppingCart.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using SnackHouse.Data;
+using System;
+using System.Collections.Generic;
+
+namespace SnackHouse.Models
+{
+    public class ShoppingCart
+    {
+        private readonly SnackHouseDbContext _snackHouseDbContext;
+
+        //injeta o contexto no construtor
+        public ShoppingCart(SnackHouseDbContext snackHouseDbContext)
+        {
+            _snackHouseDbContext = snackHouseDbContext;
+        }
+
+        //Vamos Utilizar o Guid
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            //define uma sessão acessando o contexto atual ()
+            ISession session =
+                services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            //obtem um serviço do tipo do nosso contexto 
+            var context = services.GetService<SnackHouseDbContext>();
+
+            //obtem ou gera o Id do carrinho
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            //atribui o id do carrinho na Sessão
+            session.SetString("CartId", cartId);
+
+            //retorna o carrinho com o contexto e o Id atribuido ou obtido
+            return new ShoppingCart(context)
+            {
+                ShoppingCartId = cartId
+            };
+        }
+    }
+}
