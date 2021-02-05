4c68a25abd171c12d1a7df77555903a471406f71
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2021-02-05 17:57:00 -0300

    Implementação Controladores da Area Admin [PENDENTE]
    PEDENTE: links dando página não encontrada que devem ser resolvidos.

--- SnackHouse/SnackHouse/Areas/Admin/Controllers/AdminController.cs
+++ SnackHouse/SnackHouse/Areas/Admin/Controllers/AdminController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SnackHouse.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class AdminController : Controller
     {
         public ActionResult Index()
---
+++ SnackHouse/SnackHouse/Areas/Admin/Controllers/AdminSnacksController.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SnackHouse.Data;
+using SnackHouse.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnackHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class AdminSnacksController : Controller
+    {
+        private readonly SnackHouseDbContext _context;
+
+        public AdminSnacksController(SnackHouseDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/AdminSnacks
+        public async Task<IActionResult> Index()
+        {
+            var snackHouseDbContext = _context.Snacks.Include(s => s.Category);
+            return View(await snackHouseDbContext.ToListAsync());
+        }
+
+        // GET: Admin/AdminSnacks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var snack = await _context.Snacks
+                .Include(s => s.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (snack == null)
+            {
+                return NotFound();
+            }
+
+            return View(snack);
+        }
+
+        // GET: Admin/AdminSnacks/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id");
+            return View();
+        }
+
+        // POST: Admin/AdminSnacks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,SmallDescription,LongDescription,Price,ImgUrl,ImgThumbnailUrl,IsPreferSnack,InStock,CategoryId")] Snack snack)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(snack);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", snack.CategoryId);
+            return View(snack);
+        }
+
+        // GET: Admin/AdminSnacks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var snack = await _context.Snacks.FindAsync(id);
+            if (snack == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", snack.CategoryId);
+            return View(snack);
+        }
+
+        // POST: Admin/AdminSnacks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,SmallDescription,LongDescription,Price,ImgUrl,ImgThumbnailUrl,IsPreferSnack,InStock,CategoryId")] Snack snack)
+        {
+            if (id != snack.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(snack);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SnackExists(snack.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", snack.CategoryId);
+            return View(snack);
+        }
+
+        // GET: Admin/AdminSnacks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var snack = await _context.Snacks
+                .Include(s => s.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (snack == null)
+            {
+                return NotFound();
+            }
+
+            return View(snack);
+        }
+
+        // POST: Admin/AdminSnacks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var snack = await _context.Snacks.FindAsync(id);
+            _context.Snacks.Remove(snack);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SnackExists(int id)
+        {
+            return _context.Snacks.Any(e => e.Id == id);
+        }
+    }
+}
